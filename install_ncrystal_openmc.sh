c1764ea8d8f950d5b74188810669742760816ed4
Jose Ignacio Marquez Damian <22483345+marquezj@users.noreply.github.com>
2023-01-18 10:01:08 +0100

    Add nuclear data installation

--- install_ncrystal_openmc.sh
+++ install_ncrystal_openmc.sh
@@ -5,7 +5,7 @@
 #
 
 # Install prerequisites
-sudo apt install -y git g++ cmake libhdf5-dev libpng-dev openmpi-bin libopenmpi-dev pip jupyter 
+sudo apt install -y git g++ cmake libhdf5-dev libpng-dev openmpi-bin libopenmpi-dev pip jupyter
 
 # Create installation directory
 mkdir openmc_ncrystal
@@ -18,13 +18,14 @@ cd build_ncrystal
 cmake ../ncrystal
 make -j 4
 sudo make install
+source /usr/local/setup.sh
 nctool --test
 cd ../
 
-# Install OpenMC 
-git clone --recurse-submodules https://github.com/highness-eu/openmc.git
+# Install OpenMC
+git clone --recurse-submodules https://github.com/openmc-dev/openmc.git
 cd openmc
-git checkout mixed_ncrystal
+git checkout develop
 cd ../
 mkdir build_openmc
 cd build_openmc
@@ -34,3 +35,7 @@ sudo make install
 cd ../openmc
 pip install .
 cd ../
+
+# Install OpenMC data (ENDF/B-VIII.0 library)
+wget -O endf8.xz https://anl.box.com/shared/static/uhbxlrx7hvxqw27psymfbhi7bx7s6u6a.xz
+tar -xJf endf8.xz
