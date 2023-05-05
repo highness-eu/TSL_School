f3c497aa3d15c536237307468852d6e0439966df
Jose Ignacio Marquez Damian <22483345+marquezj@users.noreply.github.com>
2023-05-05 13:41:11 -0300

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate openmc_ncrystal_vm
+source $CONDA_PREFIX/setup.sh
+export LD_LIBRARY_PATH=
+
+cd ~/openmc_ncrystal_vm
+cd mcpl
+git pull
+cd ../build_mcpl
+make -j 2 
+make install
+cd ..
+
+cd ncrystal
+git pull
+cd ../build_ncrystal
+make -j 2 
+make install
+cd ..
+
+cd openmc
+git pull
+cd ../build_openmc
+make -j 2
+make install
+cd ../openmc
+pip install .
+cd ..
