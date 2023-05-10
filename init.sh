a10c4b8b41f4647c925bb39ca2fb7638ba84ec3e
Jose Ignacio Marquez Damian <22483345+marquezj@users.noreply.github.com>
2023-05-10 17:30:32 +0200

    Fix init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-conda tsl_env
+conda activate tsl_env
 source $CONDA_PREFIX/setup.sh
 export LD_LIBRARY_PATH=
 export TSL_SCHOOL_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
