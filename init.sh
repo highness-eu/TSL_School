f51d414f7ba3696e3c8b8b407b8c6ac42f2a41cf
Jose Ignacio Marquez Damian <marquezj@ess.eu>
2023-05-10 12:24:32 +0200

    Change installation and environment name

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-conda activate openmc_ncrystal_vm
+conda tsl_env
 source $CONDA_PREFIX/setup.sh
 export LD_LIBRARY_PATH=
 export TSL_SCHOOL_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
